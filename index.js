8042a1dfcb38cf8594230dd43747e70a3f79f42d
Sifat <sifath740@gmail.com>
2025-02-09 21:34:50 +0600

    server site completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 8000;
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const movieCollection = client.db('movieDB').collection('movie');
         const favoriteMovieCollection = client.db('movieDB').collection('favorite')
@@ -151,7 +151,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
